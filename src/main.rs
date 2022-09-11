2f09b3ecb92e8e4e5f8b2d165704079a528330b6
bill_apple <bill@buster.home>
2022-09-11 18:41:41 +0300

    tree contruction (partial)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 use std::collections::HashMap;
+use std::rc::Rc;
 
 struct HuffmanEncoding {
     data: Vec<u64>,
     number_of_bits: u64
+}
+
+struct HuffmanNode {
+    weight: f64,
+    symbol: Option<char>,
+    left: Option<Rc<HuffmanNode>>,
+    right: Option<Rc<HuffmanNode>>
+}
+
+struct HuffmanUnitCode {
+    code: u64,
+    number_of_bits
 }
 
 impl HuffmanEncoding {
@@ -27,9 +40,76 @@ fn compute_frequencies(text: &String) -> HashMap<char, u32> {
     result
 }
 
+fn get_huffman_leaves(text: &String) -> Vec<HuffmanNode> {
+    let freqs = compute_frequencies(text);
+    let leaves = Vec::new();
+    for (symbol, freq in freqs) {
+        leaves.add(HuffmanNode {
+            weight: freq,
+            symbol: Some(symbol),
+            None,
+            None
+        });
+    }
+}
+
+fn construct_huffman_tree(text: &String): Rc<HuffmanNode> {
+    let leaves = get_huffman_leaves(text);
+    quick_sort(&mut leaves, &|x,y| x.weight < y.weight);
+    let intermediate_nodes = Vec::new();
+    while intermediate_nodes.len() > 1 {
+        (one, two) = take_two_smallest(leaves, intermediate_nodes);
+        let new_node = HuffmanNode {
+            weight: one.weight + two.weight,
+            symbol: None,
+            Rc::new(one),
+            Rc::new(two)
+        }
+        intermediate_nodes.push(new_node);
+    }
+
+    Rc::new(intermediate_nodes.pop())
+}
+
+fn get_huffman_encoding(text: &String) -> HashMap<char, HuffmanUnitCode> {
+    //...
+}
+
 
 fn main() {
     println!("Hello, world!");
+}
+
+fn quick_sort<T,F>(v: &mut [T], f: &F)
+    where F: Fn(&T,&T) -> bool
+{
+    let len = v.len();
+    if len >= 2 {
+        let pivot_index = partition(v, f);
+        quick_sort(&mut v[0..pivot_index], f);
+        quick_sort(&mut v[pivot_index + 1..len], f);
+    }
+}
+
+fn partition<T,F>(v: &mut [T], f: &F) -> usize
+    where F: Fn(&T,&T) -> bool
+{
+    let len = v.len();
+    let pivot_index = len / 2;
+    let last_index = len - 1;
+
+    v.swap(pivot_index, last_index);
+
+    let mut store_index = 0;
+    for i in 0..last_index {
+        if f(&v[i], &v[last_index]) {
+            v.swap(i, store_index);
+            store_index += 1;
+        }
+    }
+
+    v.swap(store_index, len - 1);
+    store_index
 }
 
 #[cfg(test)]
